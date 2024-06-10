ca6ed2d2b2b75b74b422b968d951e2fbe754c07a
Sergionx <sergio180203@gmail.com>
2024-06-09 22:33:17 -0400

    feat: Add DropdownTable component for displaying dropdown menus with options
    This commit adds the DropdownTable component, which is used for displaying dropdown menus with options. The component accepts props such as title, ariaLabel, selectionMode, selectedKeys, onSelectionChange, and options. It provides a user-friendly interface for selecting options from a dropdown menu. This addition enhances the functionality and user experience of the application.
    
    Note: The commit message has been generated based on the provided code changes.

---
+++ src/lib/components/table/DropdownTable.tsx
@@ -0,0 +1,63 @@
+import { statusOptions } from "@/app/(private)/prueba/data";
+import { capitalize } from "@/lib/utils/strings";
+import { Button } from "@nextui-org/button";
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  Selection,
+} from "@nextui-org/react";
+import { IconChevronDown } from "@tabler/icons-react";
+import React from "react";
+
+interface Option {
+  name: string;
+  uid: string;
+}
+
+interface Props<T extends Option> {
+  title: string;
+  ariaLabel: string;
+
+  selectionMode?: "single" | "multiple";
+  selectedKeys: Selection;
+  onSelectionChange: (keys: Selection) => void;
+  options: T[];
+}
+
+export default function DropdownTable<T extends Option>({
+  title,
+  ariaLabel,
+  selectionMode = "multiple",
+  selectedKeys,
+  onSelectionChange,
+  options,
+}: Props<T>) {
+  return (
+    <Dropdown>
+      <DropdownTrigger className="hidden sm:flex">
+        <Button
+          endContent={<IconChevronDown className="text-small" />}
+          variant="flat"
+        >
+          {title}
+        </Button>
+      </DropdownTrigger>
+      <DropdownMenu
+        disallowEmptySelection
+        aria-label={ariaLabel}
+        closeOnSelect={selectionMode === "single"}
+        selectedKeys={selectedKeys}
+        selectionMode={selectionMode}
+        onSelectionChange={onSelectionChange}
+      >
+        {options.map((status) => (
+          <DropdownItem key={status.uid} className="capitalize">
+            {capitalize(status.name)}
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
