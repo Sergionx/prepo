ba92d7671529c64d2c3544633224dbf2456a55f3
val <valeriazampettirubino.com>
2024-06-11 15:14:41 -0400

    login

--- src/app/login/schema.ts
+++ src/app/login/schema.ts
@@ -1,5 +1,12 @@
 import { z } from "zod";
 
+
+export interface ILoginForm{
+email: string;
+  password: string;
+}
+
+
 export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
