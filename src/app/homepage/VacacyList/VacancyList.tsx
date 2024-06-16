038323909e962d1e0b4ac33316294ab98ca65263
Sergionx <sergio180203@gmail.com>
2024-06-15 21:46:50 -0400

    feat: Update VacancyList component to use VacancySubjectName interface
    This commit updates the VacancyList component to use the VacancySubjectName interface instead of the Vacancy interface. The VacancySubjectName interface includes additional properties such as subject name, which enhances the display of vacancy information. This change improves the codebase by providing more accurate and descriptive data for each vacancy in the list.

--- src/app/homepage/VacacyList/VacancyList.tsx
+++ src/app/homepage/VacacyList/VacancyList.tsx
@@ -1,63 +1,20 @@
 "use client";
 
-import Image from "next/image";
-import { CardBody, Card } from "@nextui-org/react";
-
-import { Vacancy } from "@/lib/models/Vacancy";
+import { Vacancy, VacancySubjectName } from "@/lib/models/Vacancy";
 import ModalVacant from "./ModalVacant";
 import { useState } from "react";
 import VacancyCard from "./VacancyCard";
 
-export default function VacancyList({ vacancies }: { vacancies: Vacancy[] }) {
-  const [vacancy, setVacancy] = useState<Vacancy | null>(null);
+export default function VacancyList({
+  vacancies,
+}: {
+  vacancies: VacancySubjectName[];
+}) {
+  const [vacancy, setVacancy] = useState<VacancySubjectName | null>(null);
 
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {/* <Card isPressable>
-          <Image
-            alt="Facultad de Ingenieria"
-            className="object-cover rounded-t-lg"
-            height="300"
-            src="/facultad_ing.png"
-            width="400"
-          />
-          <CardBody>
-            <h3 className="text-lg font-semibold mb-2">
-              Algoritmos y Programacion
-            </h3>
-            <p className="text-gray-500">Conoce tus preparadores</p>
-          </CardBody>
-        </Card>
-
-        <Card isPressable>
-          <Image
-            alt="Facultad de Ciencias"
-            className="object-cover rounded-t-lg"
-            height="300"
-            src="/facultad_ciencias.jpg"
-            width="400"
-          />
-          <CardBody>
-            <h3 className="text-lg font-semibold mb-2">Psicopatologia I</h3>
-            <p className="text-gray-500">Conoce a tus preparadores</p>
-          </CardBody>
-        </Card>
-
-        <Card isPressable>
-          <Image
-            alt="Facultad de Ingenieria"
-            className="object-cover rounded-t-lg"
-            height="300"
-            src="/facultad_ing.png"
-            width="400"
-          />
-          <CardBody>
-            <h3 className="text-lg font-semibold mb-2">Fisica II</h3>
-            <p className="text-gray-500">Conoce a tus preparadores</p>
-          </CardBody>
-        </Card> */}
-
         {vacancies.map((vacancy) => (
           <VacancyCard
             key={vacancy.id}
