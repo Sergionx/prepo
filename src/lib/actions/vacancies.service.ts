e068a27b0e0ac5ebe4c01dc712e7e2e20546c18d
Sergionx <sergio180203@gmail.com>
2024-06-17 22:06:00 -0400

    refactor: Refactor createVacancy function to improve code structure and error handling
    This commit refactors the createVacancy function in the vacancies.service.ts file to improve code structure and error handling. The changes include removing unnecessary try-catch blocks, simplifying the code by removing unnecessary variable assignments, and improving error handling by returning specific error messages instead of generic error objects. This refactor enhances the codebase by making it more readable, maintainable, and robust.

--- src/lib/actions/vacancies.service.ts
+++ src/lib/actions/vacancies.service.ts
@@ -48,25 +48,21 @@ export async function createVacancy({
   description: string;
   preparers: number;
 }) {
-  try {
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const { data, error } = await supabase
-      .from("Vacancies")
-      .insert({
-        id_materia: id_materia,
-        description,
-        preparers,
-        id_coord: -1,
-      })
-      .select();
+  const { data, error } = await supabase
+    .from("Vacancies")
+    .insert({
+      id_materia: id_materia,
+      description,
+      preparers,
+      id_coord: -1,
+    })
+    .select();
 
-    if (error) return error;
+  if (error) return error;
 
-    const vacancy = data[0] as Vacancy;
+  const vacancy = data[0] as Vacancy;
 
-    return vacancy;
-  } catch (error) {
-    return error;
-  }
+  return vacancy;
 }
