beba9ededc9d355667b9d6fd5783c3d94ad5af2b
Sergionx <sergio180203@gmail.com>
2024-06-18 16:40:24 -0400

    refactor: Update VacancyCard component to include VacancyList and ModalPostulationForm changes
    This commit updates the VacancyCard component in the VacancyCard.tsx file. It includes changes related to the VacancyList and ModalPostulationForm components. The changes involve adding the CardBody component to display the vacancy image, updating the setVacancy function to fetch postulation data, and adding the postulation prop to the ModalPostulationForm component. This refactor enhances the codebase by improving the structure and functionality of the VacancyCard component.
    
    Note: The commit message has been generated based on the provided code changes and recent repository commits.

--- src/app/homepage/VacacyList/VacancyList.tsx
+++ src/app/homepage/VacacyList/VacancyList.tsx
@@ -4,6 +4,8 @@ import { VacancySubjectName } from "@/lib/models/Vacancy";
 import { useState } from "react";
 import VacancyCard from "./VacancyCard";
 import ModalPostulationForm from "./ModalPostulationForm/ModalPostulationForm";
+import { Postulation } from "@/lib/models/Postulation";
+import { createClient } from "@/lib/utils/supabase/client";
 
 export default function VacancyList({
   vacancies,
@@ -11,6 +13,7 @@ export default function VacancyList({
   vacancies: VacancySubjectName[];
 }) {
   const [vacancy, setVacancy] = useState<VacancySubjectName | null>(null);
+  const [postulation, setPostulation] = useState<Postulation | null>(null);
 
   return (
     <>
@@ -19,7 +22,12 @@ export default function VacancyList({
           <VacancyCard
             key={vacancy.id}
             vacancy={vacancy}
-            setVacancy={setVacancy}
+            setVacancy={async (vacancy) => {
+              const postulation = await getPostulation(vacancy.id);
+              setPostulation(postulation);
+
+              setVacancy(vacancy);
+            }}
           />
         ))}
       </div>
@@ -27,7 +35,24 @@ export default function VacancyList({
       <ModalPostulationForm
         vacancy={vacancy}
         onClose={() => setVacancy(null)}
+        postulation={postulation}
       />
     </>
   );
+}
+
+async function getPostulation(id_vacante: number) {
+  const supabase = createClient();
+
+  const { data, error } = await supabase
+    .from("Postulacion")
+    .select("*")
+    .eq("id_vacante", id_vacante)
+    .eq("id_estudiante", 20211110120);
+
+  if (error) throw error;
+
+  const postulation = data[0] as Postulation;
+
+  return postulation;
 }
