873f0117e823040a0148e8e92e841915dc154c3c
Sergionx <sergio180203@gmail.com>
2024-06-18 16:39:16 -0400

    feat: Add editPostulation function for updating postulations
    This commit adds the editPostulation function to the ModalPostulationForm's action.ts file. The function allows for editing an existing postulation by sending updated data to the server. It takes in the formData, subject, and id_vacante as parameters and updates the postulation with the new description and grade. This addition enhances the codebase by providing functionality for editing postulations and keeping the data up to date.
    
    Note: The commit message has been generated based on the provided code changes and recent repository commits.

--- src/app/homepage/VacacyList/ModalPostulationForm/ModalPostulationForm.tsx
+++ src/app/homepage/VacacyList/ModalPostulationForm/ModalPostulationForm.tsx
@@ -9,43 +9,25 @@ import {
   ModalFooter,
   ModalHeader,
 } from "@nextui-org/modal";
-import { useForm } from "react-hook-form";
-import { IPostulationForm, emptyForm, postulationFormSchema } from "./schema";
-import InputControl from "@/lib/components/forms/controls/InputControl";
-import TextareaControl from "@/lib/components/forms/controls/TextareaControl";
-import { zodResolver } from "@hookform/resolvers/zod";
-import useFormSubmit from "@/lib/hooks/useFormSubmit";
-import { submitPostulation } from "./action";
-import { getFormData } from "@/lib/utils/forms";
+
+import { Postulation } from "@/lib/models/Postulation";
+import PostulationForm from "./PostulationForm";
 
 interface Props {
   vacancy: VacancySubjectName | null;
+  postulation: Postulation | null;
   onClose: () => void;
 }
 
-// TODO - Intentar hacer fetch con id de estudiante para saber si se postuló y habilitar el edit
-export default function ModalPostulationForm({ vacancy, onClose }: Props) {
-  const { handleSubmit, formState, control, reset } = useForm<IPostulationForm>(
-    {
-      defaultValues: emptyForm,
-      mode: "all",
-      resolver: zodResolver(postulationFormSchema),
-    }
-  );
-
-  const { onSubmit } = useFormSubmit({
-    mode: "add",
-    addAction: (data) => {
-      if (!vacancy?.id)
-        return Promise.resolve("No se pudo enviar la postulación");
-      const formData = getFormData(data);
-      return submitPostulation(formData, vacancy.subject.nombre, vacancy?.id);
-    },
-    onSucess: () => {
-      onClose();
-      reset();
-    },
-    formState,
+export default function ModalPostulationForm({
+  vacancy,
+  postulation,
+  onClose,
+}: Props) {
+  const { postulationForm, formState } = PostulationForm({
+    onSucess: onClose,
+    postulation: postulation,
+    vacancy: vacancy,
   });
 
   return (
@@ -65,42 +47,30 @@ export default function ModalPostulationForm({ vacancy, onClose }: Props) {
             {vacancy.subject.nombre}
           </ModalHeader>
 
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <ModalBody className="sm:flex-row sm:pt-0">
-              <p className="basis-1/2 mt-2">{vacancy.description}</p>
+          <ModalBody className="sm:flex-row sm:pt-0">
+            <p className="basis-1/2 mt-2">{vacancy.description}</p>
 
-              <aside
-                className="space-y-6 basis-1/2 pt-2
+            <aside
+              className="basis-1/2 pt-2
                 sm:border-l sm:pl-2
                 max-sm:border-t max-sm:pt-2"
-              >
-                <InputControl
-                  name="grade"
-                  label={"Nota en la materia"}
-                  control={control}
-                  type="number"
-                  max={20}
-                  min={1}
-                />
+            >
+              {postulationForm}
+            </aside>
+          </ModalBody>
 
-                <TextareaControl
-                  name="description"
-                  label="Descripción"
-                  control={control}
-                />
-              </aside>
-            </ModalBody>
+          <ModalFooter>
+            <Button color="danger" variant="light" onPress={onClose}>
+              Close
+            </Button>
 
-            <ModalFooter>
-              <Button color="danger" variant="light" onPress={onClose}>
-                Close
-              </Button>
-
-              <SubmitButton
-                isLoading={formState.isLoading || formState.isSubmitting}
-              />
-            </ModalFooter>
-          </form>
+            <SubmitButton
+              form="postulation"
+              isLoading={formState.isLoading}
+              text={postulation ? "Editar" : "Crear"}
+              loadingText={postulation ? "Editando..." : "Creando..."}
+            />
+          </ModalFooter>
         </ModalContent>
       )}
     </Modal>
