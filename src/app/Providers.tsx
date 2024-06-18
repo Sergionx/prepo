5c818286bc7125eb1f6301ee66c30cb026cd1436
Sergionx <sergio180203@gmail.com>
2024-06-18 16:41:06 -0400

    refactor: Add ToastProvider to Providers component for displaying toast messages
    This commit adds the ToastProvider component to the Providers component in the Providers.tsx file. The ToastProvider is responsible for displaying toast messages in the application. By including it in the Providers component, we ensure that toast messages can be shown throughout the application. This refactor enhances the codebase by providing a centralized way to manage and display toast messages.
    
    Note: The commit message has been generated based on the provided code changes and recent repository commits.

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,11 +1,16 @@
 // app/providers.tsx
 "use client";
 
+import { ToastProvider } from "@/lib/components/ui/toast";
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter();
 
-  return <NextUIProvider navigate={router.push}>{children}</NextUIProvider>;
+  return (
+    <NextUIProvider navigate={router.push}>
+      <ToastProvider>{children}</ToastProvider>
+    </NextUIProvider>
+  );
 }
