e4216401c176b63dda50118a9c46936339bd3b0a
Angel Carrero <angel.carrero@correo.unimet.edu.ve>
2024-06-19 20:12:36 -0400

    rama angel

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -10,9 +10,9 @@ export default async function PrivateLayoutexport({
   const supabase = createClient();
 
   const { data, error } = await supabase.auth.getUser();
-  if (error || !data?.user) {
-    redirect("/login");
-  }
+  // if (error || !data?.user) {
+  //   redirect("/login");
+  // }
 
   return children;
 }
