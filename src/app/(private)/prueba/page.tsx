9497fe41562fe1285234b05e93b2262a3c19d311
Sergionx <sergio180203@gmail.com>
2024-06-20 11:50:15 -0400

    refactor: Update page.tsx to include Table component with custom cells, pagination, and sorting
    This commit updates the page.tsx file to include the Table component with custom cells, pagination, and sorting functionality. The Table component is configured with various props such as aria-label, isHeaderSticky, bottomContent, classNames, selectedKeys, selectionMode, color, sortDescriptor, topContent, onSelectionChange, and onSortChange. The TableHeader and TableBody components are also used to define the columns and items of the table respectively. This refactor enhances the codebase by improving the display and functionality of the table.

--- src/app/(private)/prueba/page.tsx
+++ src/app/(private)/prueba/page.tsx
@@ -278,46 +278,48 @@ export default function App() {
   ]);
 
   return (
-    // <div className="flex">
-    //  <Sidebar />
-    <Table
-      aria-label="Example table with custom cells, pagination and sorting"
-      isHeaderSticky
-      bottomContent={bottomContent}
-      bottomContentPlacement="outside"
-      classNames={{
-        wrapper: "max-h-[382px]",
-      }}
-      selectedKeys={selectedKeys}
-      selectionMode="multiple"
-      color="danger"
-      sortDescriptor={sortDescriptor}
-      topContent={topContent}
-      topContentPlacement="outside"
-      onSelectionChange={setSelectedKeys}
-      onSortChange={setSortDescriptor}
-    >
-      <TableHeader columns={headerColumns}>
-        {(column) => (
-          <TableColumn
-            key={column.uid}
-            align={column.uid === "actions" ? "center" : "start"}
-            allowsSorting={column.sortable}
-          >
-            {column.name}
-          </TableColumn>
-        )}
-      </TableHeader>
-      <TableBody emptyContent="No hay estudiantes postulados aún" items={items}>
-        {(item) => (
-          <TableRow key={item.id}>
-            {(columnKey) => (
-              <TableCell>{renderCell(item, columnKey)}</TableCell>
-            )}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
-    //</div>
+    <div className="p-16">
+      <Table
+        aria-label="Example table with custom cells, pagination and sorting"
+        isHeaderSticky
+        bottomContent={bottomContent}
+        bottomContentPlacement="outside"
+        classNames={{
+          wrapper: "max-h-[382px]",
+        }}
+        selectedKeys={selectedKeys}
+        selectionMode="multiple"
+        color="danger"
+        sortDescriptor={sortDescriptor}
+        topContent={topContent}
+        topContentPlacement="outside"
+        onSelectionChange={setSelectedKeys}
+        onSortChange={setSortDescriptor}
+      >
+        <TableHeader columns={headerColumns}>
+          {(column) => (
+            <TableColumn
+              key={column.uid}
+              align={column.uid === "actions" ? "center" : "start"}
+              allowsSorting={column.sortable}
+            >
+              {column.name}
+            </TableColumn>
+          )}
+        </TableHeader>
+        <TableBody
+          emptyContent="No hay estudiantes postulados aún"
+          items={items}
+        >
+          {(item) => (
+            <TableRow key={item.id}>
+              {(columnKey) => (
+                <TableCell>{renderCell(item, columnKey)}</TableCell>
+              )}
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
