0601fff68639e9ba6d9d5b3d7d3671f79c34fc64
Sergionx <sergio180203@gmail.com>
2024-06-23 15:09:52 -0400

    refactor: Update VacancyList component to include pagination and filtering
    This commit refactors the VacancyList component to include pagination and filtering functionality. It introduces the usePagination and useInputFilter hooks to handle the pagination and filtering logic. The VacancyList now displays a search input field to filter vacancies by subject name, and a pagination component to navigate through the list of vacancies. This enhancement improves the user experience by allowing easier navigation and filtering of vacancies.
    
    Note: The commit message has been generated based on the provided code changes and recent repository commits.

--- src/app/(private)/vacancies/VacacyList/VacancyList.tsx
+++ src/app/(private)/vacancies/VacacyList/VacancyList.tsx
@@ -1,12 +1,20 @@
 "use client";
 
+import { useCallback, useState } from "react";
+
+import { IconSearch } from "@tabler/icons-react";
+import { Button, Input, Pagination } from "@nextui-org/react";
+
 import { VacancySubjectName } from "@/lib/models/Vacancy";
-import { useState } from "react";
 import VacancyCard from "./VacancyCard";
 import ModalPostulationForm from "./ModalPostulationForm/ModalPostulationForm";
+
 import { Postulation } from "@/lib/models/Postulation";
 import { createClient } from "@/lib/utils/supabase/client";
+
 import { useAuth } from "@/app/(private)/AuthContext";
+import useInputFilter from "@/lib/hooks/filter/useInputFilter";
+import usePagination from "@/lib/hooks/usePagination";
 
 export default function VacancyList({
   vacancies,
@@ -16,12 +24,95 @@ export default function VacancyList({
   const [vacancy, setVacancy] = useState<VacancySubjectName | null>(null);
   const [postulation, setPostulation] = useState<Postulation | null>(null);
 
+  const {
+    inputFilterValue,
+    filteredItems: inputFilteredItems,
+    setInputFilterValue,
+  } = useInputFilter({ data: vacancies, inputKeyFilter: "subject.nombre" });
+
+  const { items, page, rowsPerPage, setRowsPerPage, setPage, pages } =
+    usePagination({
+      data: inputFilteredItems,
+      defaultRowsPerPage: 10,
+    });
+
+  console.log(inputFilteredItems);
+
   const { user } = useAuth();
+
+  const onNextPage = useCallback(() => {
+    if (page < pages) {
+      setPage(page + 1);
+    }
+  }, [page, pages, setPage]);
+
+  const onPreviousPage = useCallback(() => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  }, [page, setPage]);
+
+  const onRowsPerPageChange = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setRowsPerPage(Number(e.target.value));
+      setPage(1);
+    },
+    [setPage, setRowsPerPage]
+  );
+
+  const onSearchChange = useCallback(
+    (value?: string) => {
+      if (value) {
+        setInputFilterValue(value);
+        setPage(1);
+      } else {
+        setInputFilterValue("");
+      }
+    },
+    [setInputFilterValue, setPage]
+  );
+
+  const onClear = useCallback(() => {
+    setInputFilterValue("");
+    setPage(1);
+  }, [setInputFilterValue, setPage]);
 
   return (
     <>
+      <header className="flex flex-col gap-4 mb-6">
+        {/* REVIEW - Considerar convertirlo en un autocomplete */}
+        <Input
+          isClearable
+          className="w-full sm:max-w-[44%]"
+          placeholder="Filtrar por materia..."
+          startContent={<IconSearch />}
+          value={inputFilterValue}
+          onClear={() => onClear()}
+          onValueChange={onSearchChange}
+        />
+
+        <section className="flex justify-between items-center">
+          <span className="text-default-400 text-small">
+            {vacancies.length} vacantes en total
+          </span>
+
+          <label className="flex items-center text-default-400 text-small">
+            Vacantes por página:
+            <select
+              className="bg-transparent outline-none text-default-400 text-small"
+              value={rowsPerPage}
+              onChange={onRowsPerPageChange}
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </label>
+        </section>
+      </header>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {vacancies.map((vacancy) => (
+        {items.map((vacancy) => (
           <VacancyCard
             key={vacancy.id}
             vacancy={vacancy}
@@ -36,6 +127,39 @@ export default function VacancyList({
           />
         ))}
       </div>
+
+      <footer className="py-2 px-2 flex justify-between items-center mt-2">
+        <div className="w-1/3"></div>
+
+        <Pagination
+          isCompact
+          showControls
+          showShadow
+          color="primary"
+          page={page}
+          total={pages}
+          onChange={setPage}
+        />
+
+        <div className="hidden sm:flex w-[30%] justify-end gap-2">
+          <Button
+            isDisabled={pages === 1}
+            size="sm"
+            variant="flat"
+            onPress={onPreviousPage}
+          >
+            Previous
+          </Button>
+          <Button
+            isDisabled={pages === 1}
+            size="sm"
+            variant="flat"
+            onPress={onNextPage}
+          >
+            Next
+          </Button>
+        </div>
+      </footer>
 
       <ModalPostulationForm
         vacancy={vacancy}
