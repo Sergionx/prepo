579d2c8bdfa089d6ae12918fbf48853e175c133b
Sergionx <sergio180203@gmail.com>
2024-06-24 13:02:16 -0400

    refactor: Add ButtonDropdown component for dropdown button functionality
    This commit adds a new component called ButtonDropdown, which provides dropdown button functionality. The component includes a button and a dropdown menu, allowing users to select options from a dropdown list. This enhancement improves the user interface by providing a more intuitive and interactive way to interact with dropdown menus.
    
    Note: The commit message has been generated based on the provided code changes and recent repository commits.

---
+++ src/lib/components/ui/ButtonDropdown.tsx
@@ -0,0 +1,70 @@
+import {
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  DropdownItem,
+  ButtonGroup,
+  Button,
+  ButtonProps,
+  ButtonGroupProps,
+  DropdownProps,
+  Selection,
+} from "@nextui-org/react";
+import { IconChevronDown } from "@tabler/icons-react";
+import React from "react";
+
+export interface ButtonDropdownProps {
+  labelsMap: Record<string, string>;
+  descriptionsMap: Record<string, string>;
+
+  selectedOption: Set<string>;
+  setSelectedOption: (value: Set<string>) => void;
+
+  buttonGroupProps?: ButtonGroupProps;
+  buttonProps?: ButtonProps;
+  dropdownProps?: DropdownProps;
+}
+
+export default function ButtonDropdown({
+  labelsMap,
+  descriptionsMap,
+  selectedOption,
+  setSelectedOption,
+
+  buttonGroupProps,
+  buttonProps,
+  dropdownProps,
+}: ButtonDropdownProps) {
+  const selectedOptionValue = Array.from(selectedOption)[0];
+
+  console.log({ selectedOptionValue });
+  return (
+    <ButtonGroup {...buttonGroupProps}>
+      <Button {...buttonProps}>{labelsMap[selectedOptionValue]}</Button>
+
+      <Dropdown placement="bottom-end">
+        <DropdownTrigger>
+          <Button isIconOnly>
+            <IconChevronDown />
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu
+          disallowEmptySelection
+          aria-label="Merge options"
+          selectionMode="single"
+          selectedKeys={selectedOption}
+          // @ts-ignore
+          onSelectionChange={setSelectedOption}
+          className="max-w-[300px]"
+          {...dropdownProps}
+        >
+          {Object.keys(labelsMap).map((key) => (
+            <DropdownItem key={key} description={descriptionsMap[key]}>
+              {labelsMap[key]}
+            </DropdownItem>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
+    </ButtonGroup>
+  );
+}
