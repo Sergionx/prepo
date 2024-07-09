22929a487436e9087a84f48a13b392f72502e1c0
Sergionx <sergio180203@gmail.com>
2024-07-09 13:14:34 -0400

    New custom table

--- src/app/(pages)/(private)/(only_coordinator)/vacancies/page.tsx
+++ src/app/(pages)/(private)/(only_coordinator)/vacancies/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import { getVacancies_SubjectName } from "@/lib/actions/vacancies.service";
+import React from "react";
+import VacanciesTable from "./table";
 
-export default function VacanciesList() {
+export default async function VacanciesList() {
+  // TODO - Hacer fetch de vacantes en base al departametno
+  const vancancies = await getVacancies_SubjectName();
   return (
-    <div>VacanciesList</div>
-  )
+    <div className="p-16">
+      <VacanciesTable vacancies={vancancies} />
+    </div>
+  );
 }
