a55bcdecafcfb545a9bc8b0cc55c27202f149113
val <valeriazampettirubino.com>
2024-07-09 17:06:21 -0400

    Actualizar la barra lateral con nuevos íconos y enlaces de inicio de sesión y registro.

--- src/lib/components/layout/sidebar/messages/constants.tsx
+++ src/lib/components/layout/sidebar/messages/constants.tsx
@@ -1,9 +1,10 @@
 import {
   IconFolder,
-  IconHelpCircle,
   IconHome,
-  IconMail,
-  IconSettings,
+  IconFileCheck,
+  IconLogin2,
+  IconClipboard,
+  IconChalkboard,
 } from "@tabler/icons-react";
 import { SideNavItem } from "./types";
 
@@ -16,7 +17,7 @@ export const SIDENAV_ITEMS: SideNavItem[] = [
   {
     title: "Vacantes",
     path: "./vacancies",
-    icon: <IconFolder width="24" height="24" />,
+    icon: <IconFileCheck width="24" height="24" />,
     //submenu: true,
     // subMenuItems: [
     //   { title: "All", path: "/projects" },
@@ -26,29 +27,24 @@ export const SIDENAV_ITEMS: SideNavItem[] = [
   },
 
   {
-    title: "Messages",
-    path: "/messages",
-    icon: <IconMail width="24" height="24" />,
+    title: "Preparador",
+    path: "/preparer",
+    icon: <IconChalkboard width="24" height="24" />,
   },
-  {
-    title: "Settings",
-    path: "/settings",
-    icon: <IconSettings width="24" height="24" />,
-    submenu: true,
-    subMenuItems: [
-      { title: "Account", path: "/settings/account" },
-      { title: "Privacy", path: "/settings/privacy" },
-    ],
-  },
-  {
-    title: "Inicio Sesión",
-    path: "/login",
-    icon: <IconHelpCircle width="24" height="24" />,
-  },
+  // {
+  //   title: "Inicio de Sesión",
+  //   path: "/login",
+  //   icon: <IconLogin2 width="28" height="28" />,
+  //   // submenu: true,
+  //   // subMenuItems: [
+  //   //   { title: "Account", path: "/settings/account" },
+  //   //   { title: "Privacy", path: "/settings/privacy" },
+  //   // ],
+  // },
 
-  {
-    title: "Registarse",
-    path: "/signup",
-    icon: <IconHelpCircle width="24" height="24" />,
-  },
+  // {
+  //   title: "Registarse",
+  //   path: "/signup",
+  //   icon: <IconClipboard width="24" height="24" />,
+  // },
 ];
