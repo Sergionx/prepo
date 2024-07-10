57295fe65cb0428f177077eff07319462c984110
Sergionx <sergio180203@gmail.com>
2024-07-10 15:09:18 -0400

    refactor: Update statusPostulations to statusVacancies in vacancies/constants.ts
    This commit updates the variable name from "statusPostulations" to "statusVacancies" in the vacancies/constants.ts file. The new name accurately reflects the purpose of the variable, improving code clarity and maintainability.
    
    Note: The commit message has been generated based on the provided code changes and recent repository commits.

--- src/app/(pages)/(private)/(only_coordinator)/vacancies/page.tsx
+++ src/app/(pages)/(private)/(only_coordinator)/vacancies/page.tsx
@@ -1,13 +1,22 @@
 import { getVacancies_SubjectName } from "@/lib/actions/vacancies.service";
 import React from "react";
 import VacanciesTable from "./table";
+import { getAllSubjects } from "@/lib/actions/subjects.service";
 
 export default async function VacanciesList() {
-  // TODO - Hacer fetch de vacantes en base al departametno
-  const vancancies = await getVacancies_SubjectName();
+  // TODO - Hacer fetch de vacantes en base al departamento del coordinador
+  const [vancancies, subjects] = await Promise.all([
+    getVacancies_SubjectName(),
+    getAllSubjects(),
+  ]);
+
+  //  await new Promise((resolve) => {
+  //     setTimeout(resolve, 100000);
+  //   })
+
   return (
     <div className="p-16">
-      <VacanciesTable vacancies={vancancies} />
+      <VacanciesTable vacancies={vancancies} subjects={subjects} />
     </div>
   );
 }
