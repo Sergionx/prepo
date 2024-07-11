1c10813d00d951078c2884b8b6dd61eebf24d758
Sergionx <sergio180203@gmail.com>
2024-07-10 21:59:58 -0400

    cansadito cansadito

--- src/app/(pages)/(private)/preparer/page.tsx
+++ src/app/(pages)/(private)/preparer/page.tsx
@@ -1,13 +1,13 @@
-import { Skeleton } from "@nextui-org/react";
 import React, { Suspense } from "react";
 import PreparerListFetch from "./PreparerList/PreparerListFetch";
+import Skeleton from "./PreparerList/Skeleton";
 
 export default function Page() {
   return (
     <main className="py-12 px-6">
       <div className="flex-grow px-6">
         <div className="text-center">
-          <h1 className="text-3xl font-bold mb-6">Bienvenido a Prepo</h1>
+          <h1 className="text-3xl font-bold mb-6">Conoce a tus prepardores!</h1>
 
           <Suspense fallback={<Skeleton />}>
             <PreparerListFetch />
